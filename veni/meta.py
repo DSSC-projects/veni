d38322423eec62dae671572cab1057e74b0e371d
Dario Coscia <93731561+dario-coscia@users.noreply.github.com>
2022-06-23 19:20:55 +0200

    Update meta.py

--- veni/meta.py
+++ veni/meta.py
@@ -2,6 +2,7 @@ __all__ = [
     '__project__',
     '__title__',
     '__author__',
+    '__copyright__',
     '__license__',
     '__version__',
     '__maintainer__',
@@ -10,6 +11,7 @@ __all__ = [
 __project__ = 'veni'
 __title__ = "veni"
 __author__ = "Dario Coscia, Alessandro Pierro, Francesco Tomba"
+__copyright__ = "Copyright 2022, veni authors"
 __license__ = "MIT"
 __version__ = "0.0.1"
 __maintainer__ = __author__
